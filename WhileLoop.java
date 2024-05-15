f71ce3303c44d8ae2c6d012fb22b1652459e1241
umantrip <umantrip25@gmail.com.com>
2024-05-15 01:58:29 -0400

    looping

---
+++ WhileLoop.java
@@ -0,0 +1,22 @@
+package practicejava;
+
+public class WhileLoop {
+
+	public static void main(String[] args) 
+	{
+		int age=0;
+		while(age<5)
+			
+		{
+			System.out.println(age);
+			age++;
+		}
+
+		}
+
+	
+
+}
+
+	
+
